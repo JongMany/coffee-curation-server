b985456114018e1d1de7ede6f96695a144ce5da8
JongMany <blackberry1114@naver.com>
2025-03-16 21:58:20 +0900

    feat: rbac decorator + rbac guard 추가

---
+++ apps/gateway/guard/rbac.guard.ts
@@ -0,0 +1,27 @@
+import { Role } from '@app/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RBACGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+  canActivate(context: ExecutionContext): boolean {
+    const role = this.reflector.get<Role>(RBACGuard, context.getHandler());
+
+    // Role Enum에 해당되는 값이 데코레이터에 들어갔는지 확인하기
+    if (!Object.values(Role).includes(role)) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+
+    // Auth Guard 통과 여부 체크
+    const user = request.user;
+
+    if (!user) {
+      return false;
+    }
+
+    return user.role <= role;
+  }
+}
--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,5 +1,6 @@
 export * from './constants';
 export * from './decorator';
+export * from './entity';
 export * from './filter';
 export * from './grpc';
 export * from './types';
